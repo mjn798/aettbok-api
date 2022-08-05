e57ffe81b8db72721aaa5fef72436b9d8b0b1155
Michael J. Neumann <53601513+mjn798@users.noreply.github.com>
2022-08-05 20:32:15 +0200

    tokenvalidation

--- src/common/tokenvalidator.js
+++ src/common/tokenvalidator.js
@@ -1,8 +1,9 @@
 // configure imports
 
 const aettbok = require('./aettbok')
-const jwt     = require('jsonwebtoken')
 const redis   = require('../db/redis')
+const https   = require('https')
+const jwt     = require('jsonwebtoken')
 
 
 
@@ -39,7 +40,7 @@ async function validateToken(req, res, next) {
     }
 
     // get cached key
-    redis.getGoogleApiKey(authenticationDetails.header.kid)
+    getGoogleApiKey(authenticationDetails.header.kid)
     .then(result => jwt.verify(authenticationDetails.token, result, { algorithms: [authenticationDetails.header.alg] }, (error, data) => {
 
         // invalid token = (401)
@@ -52,6 +53,56 @@ async function validateToken(req, res, next) {
     }))
     .catch(error => aettbok.sendError(req, res, error, 'aettbok:validateToken:googleApiKeyError'))
 
+}
+
+// get Google API key used to sign the jwt
+
+function getGoogleApiKey(apikey) {
+    return new Promise((resolve, reject) => {
+
+        return redis.getEntry(apikey)
+        .then(result => {
+
+            // key has been cached
+            if (result !== null) { return resolve(result) }
+
+            // key has not been cached
+            return getApiKeysFromGoogle()
+            .then(result => {
+
+                // missing Google API key = (500)
+                if (!result[apikey]) { return reject(500) }
+
+                // cache and set new key
+                redis.setEntry(apikey, result[apikey], process.env.REDIS_GOOGLEAPIKEY_SEC)
+                return resolve(result[apikey])
+
+            })
+            .catch(error => reject(error))
+
+        })
+        .catch(error => reject(error))
+
+    })
+}
+
+// get missing key directly from Google's API
+
+function getApiKeysFromGoogle() {
+    return new Promise((resolve, reject) => {
+
+        return https.get(process.env.GOOGLE_APIS, (response) => {
+
+            let body = ''
+
+            response.setEncoding('utf-8')
+            response.on('data',  (chunk) => body += chunk)
+            response.on('end',   () => resolve(JSON.parse(body)))
+
+        })
+        .on('error', () => reject(500))
+
+    })
 }
 
 
--- src/db/redis.js
+++ src/db/redis.js
@@ -1,17 +1,11 @@
-// configure imports
+// configure and connect Redis Client
 
-const redis = require('redis')
-const https = require('https')
-
-// configure Redis Client connection
-
-const client = redis.createClient(6379)
+const redis  = require('redis')
+const client = redis.createClient(process.env.REDIS_PORT)
 .on('connect',      () => console.info('redisClient:connected'))
 .on('end',          () => console.info('redisClient:disconnected'))
 .on('reconnecting', () => console.info('redisClient:reconnecting'))
 .on('error',        (error) => { console.error('redisClient:error', error) });
-
-// connect Redis Client
 
 (async () => {
     try      { await client.connect() }
@@ -20,74 +14,13 @@ const client = redis.createClient(6379)
 
 
 
-/* TOKEN VALIDATION */
-
-
-
-// get or set Google API Key
-
-function getGoogleApiKey(apikey) {
-    return new Promise((resolve, reject) => {
-
-        return client.get(apikey)
-        .then(async (key, error) => {
-
-            // Redis Client error = (500)
-            if (error) { return reject(500) }
-
-            // found cached key
-            if (key) { return resolve(key) }
-
-            // did not find cached key
-            return getGoogleApiKeys()
-            .then(result => {
-
-                // missing key from Google API = (500)
-                if (!result[apikey]) { return reject(500) }
-
-                // cache and set new key
-                client.setEx(apikey, process.env.REDIS_NODECACHE_SEC, result[apikey])
-                return resolve(result[apikey])
-
-            })
-            .catch(error => reject(error))
-
-        })
-        .catch(() => reject(500))
-
-    })
-
-}
-
-// get Google API Keys for JWT validation
-
-function getGoogleApiKeys() {
-    return new Promise((resolve, reject) => {
-
-        return https.get('https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com', (response) => {
-
-            let body = ''
-
-            response.setEncoding('utf-8')
-            response.on('data',  (chunk) => body += chunk)
-            response.on('end',   () => resolve(JSON.parse(body)))
-
-        })
-        .on('error', () => reject(500))
-
-    })
-}
-
-
-
 /* NODE CACHING */
 
 
 
-// function setEntry(id, value) { return client.set(id, value) }
-function setEntry(id, value) { return client.setEx(id, process.env.REDIS_NODECACHE_SEC, JSON.stringify(value)) }
-
 function deleteEntry(id) { return client.del(id) }
+
+function setEntry(id, value, expiration = process.env.REDIS_NODECACHE_SEC) { return client.setEx(id, expiration, JSON.stringify(value)) }
 
 function getEntry(id) {
     return new Promise((resolve, reject) => {
@@ -116,6 +49,5 @@ function getEntry(id) {
 module.exports = {
     deleteEntry,
     getEntry,
-    getGoogleApiKey,
     setEntry,
 }
